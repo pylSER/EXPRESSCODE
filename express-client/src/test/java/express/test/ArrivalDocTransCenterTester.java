4f68588b6f20bf6bce10c0bd18d20a0d92bd3aae
pylSER <pyl14@software.nju.edu.cn>
2015-12-25 23:15:41 +0800

    express v1225

---
+++ express-client/src/test/java/express/test/ArrivalDocTransCenterTester.java
@@ -0,0 +1,35 @@
+package express.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import express.businessLogic.documentBL.ArrivalDocTransCenter;
+import express.po.GoodsArrivalStatus;
+import express.rmi.RMIClient;
+import express.vo.ArrivalDocTransCenterVO;
+
+public class ArrivalDocTransCenterTester {
+
+	//@Test
+//	public void test() {
+//		try{
+//			RMIClient.init();
+//		}catch(Exception e){
+//			e.printStackTrace();
+//		}
+//		
+//		ArrivalDocTransCenterVO vo=new ArrivalDocTransCenterVO("0000000001", "2015-12-29", "0250201512290000000", "-1", "南京", GoodsArrivalStatus.Complete);
+//		ArrivalDocTransCenter adtc=new ArrivalDocTransCenter();
+//		
+//		assertEquals(true,adtc.addArrivalDoc(vo));
+//		
+//		adtc.endArrivalDoc();
+//		
+//	}
+		
+	
+
+}
